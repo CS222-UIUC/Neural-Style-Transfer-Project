b39bc271a8e6be5311139bf8ee9fb09cf92829b4
Jacob Shalabi <jshalabi03@gmail.com>
2022-10-06 17:37:09 -0500

    Set up jest, initialized empty model files with tensor flow js, created example test

---
+++ web-app/model/Main.js
@@ -0,0 +1,11 @@
+import * as tf from '@tensorflow/tfjs';
+
+let constant_image_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Golden_Gate_Bridge_from_Battery_Spencer.jpg/640px-Golden_Gate_Bridge_from_Battery_Spencer.jpg';
+let style_image_url = 'https://upload.wikimedia.org/wikipedia/commons/0/0a/The_Great_Wave_off_Kanagawa.jpg';
+let output_image_size = 384;
+
+const TestComponent = () => {
+    return output_image_size;
+}
+
+export default TestComponent;
