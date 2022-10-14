242eac5ec09964f8d8be0b0289ab6fcf34a60069
Jacob Shalabi <jshalabi03@gmail.com>
2022-10-13 21:00:28 -0500

    Working style transfer demo

--- web-app/model/Main.js
+++ web-app/model/Main.js
@@ -1,11 +1,49 @@
-import * as tf from '@tensorflow/tfjs';
+// import * as tf from '@tensorflow/tfjs';
 
-let constant_image_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Golden_Gate_Bridge_from_Battery_Spencer.jpg/640px-Golden_Gate_Bridge_from_Battery_Spencer.jpg';
-let style_image_url = 'https://upload.wikimedia.org/wikipedia/commons/0/0a/The_Great_Wave_off_Kanagawa.jpg';
-let output_image_size = 384;
+const content_image_url = 'content_image.jpg';
+const style_image_url = 'style_image.jpg';
 
-const TestComponent = () => {
-    return output_image_size;
+// const model = tf.loadGraphModel('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2');
+const model = await tf.loadGraphModel('style_transfer_tfjs/model.json');
+
+function preprocess(imgData) {
+    return tf.tidy(() => {
+
+        // Convert image to 3D tensor
+        let tensor = tf.browser.fromPixels(imgData, numChannels=3)
+
+        // Normalize tensor
+        const offset = tf.scalar(255.0)
+        const normalized = tensor.div(offset)
+
+        // Add dimension to achieve desired tensor shape
+        const batched = normalized.expandDims(0)
+        return batched
+
+    })
 }
 
-export default TestComponent;
+// Retrieve images
+let contentImage = document.getElementById('contentImage');
+let styleImage   = document.getElementById('styleImage');
+
+// Process images as tensors
+let contentImageTensor = preprocess(contentImage);
+let styleImageTensor   = preprocess(styleImage);
+
+// Pass images through model to train
+let result = model.execute([contentImageTensor, styleImageTensor]);
+
+// Remove extra dimension from batched result
+let outputImage = tf.squeeze(result);
+
+let canvas = document.getElementById('stylizedImage');
+
+// Draw output image to canvas
+tf.browser.toPixels(outputImage, canvas);
+
+// const TestComponent = () => {
+//     return 384;
+// }
+
+// export default TestComponent;
