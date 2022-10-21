9037e4f2229a1f44ebb922d8fb568a7865795dd2
Jacob Ko <jacobko2@illinois.edu>
2022-10-21 14:01:21 -0500

    Added Social Media bar with some functionality

---
+++ web-app/src/components/Socials/SocialBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+// import Card from "../UI/Card.js";
+import { FacebookIcon, FacebookShareButton, EmailShareButton, EmailIcon, TwitterIcon, TwitterShareButton } from "react-share";
+import classes from "./SocialBar.module.css";
+import SnapchatButton from "./Snap.js";
+
+const SocialBar = () => {
+    const Url = window.location.href;
+
+    return (
+        <div className={classes.bar}>
+            <TwitterShareButton url={Url} title="Look at my cool picture!" id="button twitter">
+
+                <TwitterIcon size={30} round={true}/>
+            </TwitterShareButton>
+            <FacebookShareButton url={Url} quote="Look at my cool picture!" id="button facebook">
+
+                <FacebookIcon size={30} round={true}/>
+            </FacebookShareButton>
+            <EmailShareButton url={Url} id="button email">
+
+                <EmailIcon size={30} round={true}/>
+            </EmailShareButton>
+            <div className={classes.snap}>
+                <SnapchatButton/>
+            </div>
+        </div>
+    );
+};
+
+export default SocialBar;
