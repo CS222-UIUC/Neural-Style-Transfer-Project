3eacbc99a339da3e9f97c87f0f61d32a85efd4c6
Ritul Soni <rsoni27@illinois.edu>
2022-10-21 21:23:06 -0500

    Removed temp from git ignore

---
+++ web-app/src/temp.js
@@ -0,0 +1,44 @@
+
+//</div><Parallax pages={4}>
+  //</Parallax><ParallaxLayer
+// offset={0}
+// speed={1}
+// factor={1}
+
+// style={{
+//   backgroundImage: `url(${BlueDot})`,
+//   backgroundSize: `cover`,
+// }}
+
+// >
+// <h2> Welcome to Neural Style Transfer Site</h2>
+// </ParallaxLayer>
+
+// <ParallaxLayer offset={1} speed={0.5}>
+// <h2> What yall know about thus saan!</h2>
+// </ParallaxLayer>
+
+// </Parallax>
+
+
+
+
+/* <Parallax pages={4}>
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={1}
+          style={{
+            backgroundImage: `url(${tech})`,
+            backgroundSize: `cover`,
+          }}
+        >
+          <h2> Welcome to Neural Style Transfer Site</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0.5}>
+          <h2> What yall know about thus saan!</h2>
+        </ParallaxLayer>
+      </Parallax> */
+
+      //Do not need this is gitignore
