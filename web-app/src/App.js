5bc6787ebe8f2c6e4b55bf026573b35b2e6f50eb
Ritul Soni <rsoni27@illinois.edu>
2022-10-21 19:50:24 -0500

    Created NavBar

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,16 +1,15 @@
-
-
 import React, { useState } from "react";
 
 import AddImage from "./components/Images/AddImage.js";
 import ImagesList from "./components/Images/ImagesList.js";
 import PickStyles from "./components/Styles/PickStyles.js";
 import SocialBar from "./components/Socials/SocialBar.js";
+import NavBar from "./components/NavBar/Navbar.js";
 import classes from "./App.module.css";
-//import tech from "./components/Styling/GIF/tech.gif";
-//import BlueDot from "./components/Styling/PNG/Blue Dot .jpg"
-//import BlackLines from "./components/Styling/PNG/Black lines.jpg"
-//import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import tech from "./components/Storage/GIF/tech.gif";
+import BlueDot from "./components/Storage/PNG/Blue Dot .png";
+import BlackLines from "./components/Storage/PNG/Black lines.png";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function App() {
   const [imagesList, setImagesList] = useState([]);
@@ -37,10 +36,9 @@ function App() {
   };
 
   return (
-  
-
 
     <div className={classes.wrapper}>
+        <NavBar />
       <div className={classes.control}>
 
         <AddImage onAddImage={addImageHandler} id="AddImage-component" />
@@ -48,41 +46,16 @@ function App() {
           onChangeStyle={changeStyleHandler}
           data-testid="PickStyles-component"
         />
-
-
       </div>
-      <ImagesList images={imagesList} data-testid="ImagesList-component" />
+  
+     <ImagesList images={imagesList} data-testid="ImagesList-component" />
+      
       <div className={classes.bar}>
-        <SocialBar/>
-      </div> 
+        <SocialBar />
+      </div>
+
     </div>
   );
 }
 
 export default App;
-
-// <Parallax pages={4}>
-// <ParallaxLayer 
-// offset={0}
-// speed={1}
-// factor={1} 
-
-// style={{
-//   backgroundImage: `url(${BlueDot})`,
-//   backgroundSize: `cover`,
-// }}
-
-
-// >
-// <h2> Welcome to Neural Style Transfer Site</h2>
-// </ParallaxLayer>
-
-// <ParallaxLayer offset={1} speed={0.5}>
-// <h2> What yall know about thus saan!</h2>
-// </ParallaxLayer>
-
-// </Parallax>
-
-
-
-
---
+++ web-app/src/components/NavBar/Pages.js
@@ -0,0 +1,26 @@
+export const MenuItems = [
+    {
+        title: ' Home',
+        url: '#',
+        cName: 'nav-links',
+        icon: <i class = {'fa-solid fa-house'}></i>
+    },
+    {
+        title: ' Neural Style Transfer',
+        url: '#',
+        cName: 'nav-links',
+        icon: <i class="fa-solid fa-pencil"></i>
+    },
+    {
+        title: 'Learn More',
+        url: '#',
+        cName: 'nav-links',
+        icon: <i class="fa-solid fa-book"></i>
+    },
+    {
+        title: 'About Us',
+        url: '#',
+        cName: 'nav-links',
+        icon: <i class="fa-solid fa-people-group"></i>
+    },
+]
