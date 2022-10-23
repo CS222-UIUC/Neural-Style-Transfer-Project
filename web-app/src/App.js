1f937c389d4a7c03c030736a911bf7d37a339766
Jacob Shalabi <jshalabi03@gmail.com>
2022-10-23 03:56:53 -0500

    Created test draw to canvas component

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,17 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import AddImage from "./components/Images/AddImage.js";
 import ImagesList from "./components/Images/ImagesList.js";
 import PickStyles from "./components/Styles/PickStyles.js";
 import SocialBar from "./components/Socials/SocialBar.js";
+import loadModel from "./model/Main.js";
+import Draw from "./model/Draw.js"
+import { doStyleTransfer } from "./model/Main.js";
 
 import classes from "./App.module.css";
+import Button from "./components/UI/Button.js";
 
 function App() {
+
+  const ref = useRef(null)
+  useEffect(() => {
+    const el2 = ref.current
+    console.log(el2);
+  })
+
   const [imagesList, setImagesList] = useState([]);
   const [style, setStyle] = useState(undefined);
 
   const addImageHandler = (newLabel, newImg) => {
+    // let stylizedImage = doStyleTransfer()
     setImagesList((prevImagesList) => {
       return [
         ...prevImagesList,
@@ -31,6 +43,11 @@ function App() {
     console.log("handler ran", style);
   };
 
+  async function modelHandler() {
+    const model = await loadModel();
+    console.log(model);
+  }
+
   return (
     <div className={classes.wrapper}>
       <div className={classes.control}>
@@ -44,6 +61,12 @@ function App() {
       <div className={classes.bar}>
         <SocialBar/>
       </div> 
+      <div>
+        <Draw></Draw>
+        <Button onClick={modelHandler}> 
+          Load model
+        </Button>
+      </div>
     </div>
   );
 }
--- web-app/src/App.test.js
+++ web-app/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 import loadModel from '../model/Main'
+import Draw from './model/Draw.js';
 
-// test('Model properly loads', () => {
-//   let res_model = loadModel();
-//   expect(res_model).not.toBeNull();
-// });
+test('Test draw', () => {
+    render(<Draw />);
+
+    const testCanvas = screen.getByTestId('testCanvas')
+    const drawButton = screen.getByText('Draw')
+    fireEvent.click(drawButton)
+
+    const context = canvas.getContext("2d");
+    const imageData = context.getImageData(0, 0, 1, 1)
+    expect(imageData.colorSpace).toBe('srgb')
+});
