3c6d45aedc450b9bd3dc91252e148ee84edefb3c
Jacob Ko <jacobko2@illinois.edu>
2022-11-09 16:39:00 -0600

    Fixing merge conflicts

---
+++ web-app/src/model/Recieve.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react"
+import './Recieve.css'
+const Recieve = ({route}) => {
+
+    const [data, setData] = useState(null);
+
+    async function fetchData(route) {
+        const res = await fetch(`/${route}`);
+        const data = await res.json();
+        setData(data.message);
+    }
+
+    useEffect(() => {
+        fetchData(route);
+    }, [route]);
+
+    return (
+        <p data-testid="apiRecieve">{!data ? "Loading..." : data}</p>
+    );
+
+}
+
+export default Recieve;
