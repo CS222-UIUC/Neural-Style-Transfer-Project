fe7a20ad6c034089eef5bdaed1d5b6ea12339fe9
Daniel Ku <DK@MacBook-Pro-10.local>
2022-11-11 02:18:19 -0600

    feat: wrote tests for some components

--- web-app/src/tests/PickStyles.test.js
+++ web-app/src/tests/PickStyles.test.js
@@ -2,3 +2,11 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import PickStyles from "../components/Styles/PickStyles";
+
+describe("PickStyles.js Testing", () => {
+  test("Selection dropdown exists", () => {
+    render(<PickStyles />);
+    const select = document.getElementById("Select");
+    expect(select).toBeInTheDocument();
+  });
+});
