967fed93a5cc446ba8d149492a9d32656d26ab42
Daniel Ku <DK@MacBook-Pro-10.local>
2022-11-11 02:16:10 -0600

    feat: wrote some tests

--- web-app/src/tests/AddImage.test.js
+++ web-app/src/tests/AddImage.test.js
@@ -2,3 +2,11 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import AddImage from "../components/Images/AddImage";
+
+describe("AddImage.js Testing", () => {
+  test("Form exists", () => {
+    render(<AddImage />);
+    const form = document.getElementById("Form");
+    expect(form).toBeInTheDocument();
+  });
+});
