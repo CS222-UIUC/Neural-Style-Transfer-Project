0de219566b40b1a038cea8a52e1168e7573fd2b4
Jacob Ko <jacobko2@illinois.edu>
2022-11-18 14:01:38 -0600

    Restructured Social Nav Button

--- web-app/src/components/Socials/SocialNav.js
+++ web-app/src/components/Socials/SocialNav.js
@@ -1,19 +1,32 @@
 import React from "react";
 import SocialBar from "./SocialBar.js";
 
-import classes from "./SocialNav.module.css"
+// import classes from "./SocialNav.module.css"
+
+import "./SocialNav.css";
 
 const SocialNav = ({open, setOpen, items}) => {
 
+    // return (
+    //     <div className={classes.wrapper}>
+    //         <li className={classes.button} onClick={setOpen} id="SocialNavButton">
+    //             {/* <img src={speech} alt="Socials" width="100px" height="100px"></img> */}
+    //             Post to Social Media!
+    //         </li>
+    //         {open && (
+    //             <SocialBar className={classes.dropdown}/>
+    //         )}
+    //     </div>
+    // );
+
     return (
-        <div className={classes.wrapper}>
-            <button className={classes.button} onClick={setOpen} id="SocialNavButton">
-                {/* <img src={speech} alt="Socials" width="100px" height="100px"></img> */}
-                Post to Social Media!
-            </button>
-            {open && (
-                <SocialBar className={classes.dropdown}/>
-            )}
+        <div>
+            <li class="dropdown dropdown-6" onClick={setOpen}>
+            Share To Social Media!
+                <ul class="dropdown_menu dropdown_menu--animated dropdown_menu-6" >
+                    <SocialBar></SocialBar>
+                </ul>
+            </li>
         </div>
     );
 }
