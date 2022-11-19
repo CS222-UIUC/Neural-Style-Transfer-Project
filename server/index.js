0cbac843cdcca2997c8f0e547ea4146c2b4118af
Daniel Ku <DK@MacBook-Pro-10.local>
2022-11-19 15:53:11 -0600

    fix: server/index.js receive spelling

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Backend query recieved." });
+  res.json({ message: "Backend query received." });
 });
 
 app.listen(PORT, () => {
