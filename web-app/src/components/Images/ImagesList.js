7298547a5d1ed9af99fc31c2a13e66790d0b59ef
Jacob Ko <jacobko2@illinois.edu>
2022-12-02 16:35:39 -0600

    Fixed Functionality for Download Button

--- web-app/src/components/Images/ImagesList.js
+++ web-app/src/components/Images/ImagesList.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Card from "../UI/Card.js";
 import classes from "./ImagesList.module.css";
-import DownloadButton from "../Socials/Download.js";
+// import DownloadButton from "../Socials/Download.js";
 
 const ImagesList = (props) => {
   console.log(props.images);
+
+  const downloadImage = () => {
+    // Get canvas element
+    let canvas = document.getElementById("canvas");
+
+    // Image URL in Base64 (error when trying to download)
+    var image = canvas.toDataURL("image/png");
+
+    var a = document.createElement("a"); //Create <a>
+    a.href = image; //Image Base64 Goes here
+    a.download = "Image.png"; //File name Here
+    a.click(); //Downloaded file
+    console.log("Downloaded Image");
+  }
+
   return (
     <Card className={classes.images}>
       <ul>
@@ -13,8 +28,8 @@ const ImagesList = (props) => {
             <label>{image.label}</label>
             <img src={image.img} alt="current" width="100" height="100" />
             <img src={image.style} alt="style" width="100" height="100" />
-            <canvas ref={image.canvasRef}></canvas>
-            <DownloadButton img={image.style} />
+            <canvas id="canvas" ref={image.canvasRef}></canvas>
+            <button onClick={downloadImage}>Download</button>
             <button
               onClick={() => {
                 console.log("hello");
